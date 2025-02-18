2b0f8c5ad674f5d6327461d8325e8a790d398c6d
Mats Sjöberg <mats.sjoberg@csc.fi>
2025-02-18 09:53:21 +0200

    Updated LUMI script

--- run-lumi8.sh
+++ run-lumi8.sh
@@ -10,25 +10,37 @@
 module use /appl/local/csc/modulefiles/
 module load pytorch/2.5
 
+# Where to store the huge models
+# For example Deepseek-R1-Distill-Llama-70B requires 132GB
 export HF_HOME=/scratch/project_462000007/mvsjober/hf-cache
 
-# Deepseek-r1-70b
+# Where to store the vLLM server log
+VLLM_LOG=/scratch/project_462000007/mvsjober/vllm-logs/${SLURM_JOB_ID}.log
+mkdir $(dirname $VLLM_LOG)
+
 MODEL="deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
+#MODEL="deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
 
 python -m vllm.entrypoints.openai.api_server --model=$MODEL \
        --tensor-parallel-size 4 \
+       --pipeline-parallel-size 2 \
        --max-model-len 32768 \
-       --enforce-eager &
+       --enforce-eager > $VLLM_LOG &
 
 VLLM_PID=$!
+
+echo "Starting vLLM process $VLLM_PID - logs go to $VLLM_LOG"
 
 # Wait until vLLM is running properly
 sleep 20
 while ! curl localhost:8000 >/dev/null 2>&1
 do
+    # catch if vllm has crashed
+    if [ -z "$(ps --pid $VLLM_PID --no-headers)" ]; then
+        exit
+    fi
     sleep 10
 done
-
 
 curl localhost:8000/v1/completions -H "Content-Type: application/json" \
      -d "{\"prompt\": \"What would be like a hello world for LLMs?\", \"temperature\": 0, \"max_tokens\": 100, \"model\": \"$MODEL\"}" | json_pp
