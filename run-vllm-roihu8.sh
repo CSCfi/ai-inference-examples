b6ee9488790a8e8cb65803e8c84b61e2cca30aee
Mats Sjöberg <mats.sjoberg@csc.fi>
2026-09-25 10:23:36 +0300

    Updated Roihu scripts a bit

--- run-vllm-roihu8.sh
+++ run-vllm-roihu8.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-#SBATCH --argos=no
 #SBATCH --account=project_2001659
-#SBATCH --partition=gpumedium
+#SBATCH --partition=gpularge
 #SBATCH --time=30
 #SBATCH --tasks-per-node=1
 #SBATCH --gres=gpu:gh200:4
 #SBATCH --nodes=2
-#SBATCH --mem=240G
 #SBATCH --cpus-per-task=288
+#SBATCH --mem=868344
 
 module purge
-module load csc-tools
-module load python-vllm/0.19
+module load python-vllm/0.29.0
 
 MODEL=Qwen/Qwen3-32B
 
-# We are putting the cache in the ramdisk, stored in
-# memory. Alternatively store it to the project's scratch.
+# We are putting the cache on the local NVME drive.
+# Alternatively store it to the project's scratch.
 
 #export HF_HOME=/scratch/$SLURM_JOB_ACCOUNT/$USER/hf-cache/
-export HF_HOME=/dev/shm/$USER/hf-cache
-export TORCHINDUCTOR_CACHE_DIR=/dev/shm/$USER/
+export HF_HOME=$TMPDIR/hf-cache
+export TORCHINDUCTOR_CACHE_DIR=$TMPDIR/torch-cache
 mkdir -p $HF_HOME
 
 export MASTER_ADDR=${MASTER_ADDR:-$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)}
